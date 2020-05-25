108d1e7bc176e09e8c39d3fe321fc48860311ab2
Alan <alansal_11@hotmail.com>
2020-05-25 18:17:45 -0500

    Se agregó funcionalidad al botón de registro

--- app/src/main/java/com/example/fazt/MainActivity.java
+++ app/src/main/java/com/example/fazt/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.fazt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegistrar = (Button) findViewById(R.id.registrate);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registro();
+            }
+        });
     }
+
+    public void registro() {
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
+    }
+
+    ;
 }
