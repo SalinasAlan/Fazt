9a4963069d8cfed8bbe6779e860a43418fe1ab7d
Alan <alansal_11@hotmail.com>
2020-05-25 21:20:18 -0500

    Se importaron librerias

--- app/src/main/java/com/example/fazt/MainActivity.java
+++ app/src/main/java/com/example/fazt/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnRegistrar;
